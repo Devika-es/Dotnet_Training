f15d942dbced11241c6584fb4324c16b81c828a9
Devika E S <devikaes17@gmail.com>
2025-12-27 20:43:14 +0530

    LateFeeCalculation added

---
+++ LateFeeCalculation/Book.cs
@@ -0,0 +1,41 @@
+using System;
+
+
+namespace LateFeeCalculation;
+
+public class Book
+{
+    public string title;
+    public string author;
+    public int numPages;
+    public DateTime dueDate;
+    public DateTime returnedDate;
+    public Book()
+    {
+        
+    }
+    public Book(string title,string author,int numPages,DateTime dueDate,DateTime returnedDate)
+    {
+        this.title=title;
+        this.author=author;
+        this.numPages=numPages;
+        this.dueDate=dueDate;
+        this.returnedDate=returnedDate;
+    }
+    public double AveragePagesReadPerDay(int daysToRead)
+    {
+        double avgPages=numPages/daysToRead;
+        return avgPages;
+        
+    }
+    
+    public double CalculateLateFee(double dailyLateFeeRate)
+    {
+        TimeSpan timespan=returnedDate-dueDate;
+        double noOfDaysLate=timespan.TotalDays;
+        double lateFee=noOfDaysLate * dailyLateFeeRate;
+        return lateFee;
+    }
+
+
+}
---
+++ LateFeeCalculation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+namespace LateFeeCalculation;
+class Program
+{
+    public static void Main()
+    {
+        
+        System.Console.WriteLine("Enter the title");
+        string title=Console.ReadLine();
+
+        System.Console.WriteLine("Enter the author");
+        string author=Console.ReadLine();
+
+        System.Console.WriteLine("Enter the number of pages");
+        int numPages=Int32.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("Enter the due date");
+        string input1=Console.ReadLine();
+        //here DateTime is in format "MM/dd/yyyy so explicitly need to convert by taking input as string
+        DateTime dueDate;
+        DateTime.TryParseExact(
+            input1,
+            "MM/dd/yyyy",
+            CultureInfo.InvariantCulture,
+            DateTimeStyles.None,
+            out dueDate
+        );
+
+        System.Console.WriteLine("Enter the return date");
+        string input2= Console.ReadLine();
+
+        DateTime returnedDate;
+        DateTime.TryParseExact(
+            input2,
+            "MM/dd/yyyy",
+            CultureInfo.InvariantCulture,
+            DateTimeStyles.None,
+            out returnedDate
+        );
+        System.Console.WriteLine("Enter the days to read");
+        int daysToRead=Int32.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("Enter the daily late feeRate");
+        double dailyLateFeeRate=double.Parse(Console.ReadLine());
+        Book bookObj=new Book(title,author,numPages,dueDate,returnedDate);
+
+        System.Console.WriteLine($"Average Pages Read Per Day: {bookObj.AveragePagesReadPerDay(daysToRead)}");
+        System.Console.Write($"Late Fee: {bookObj.CalculateLateFee(dailyLateFeeRate)}");
+        
+
+    }
+}
