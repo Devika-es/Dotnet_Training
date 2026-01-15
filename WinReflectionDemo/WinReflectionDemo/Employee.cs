c288c03d277561c12162ab7c7a61b210dff4181e
Devika E S <devikaes17@gmail.com>
2026-01-15 11:44:19 +0530

    WinReflectionDemo

---
+++ WinReflectionDemo/WinReflectionDemo/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinReflectionDemo
+{
+    public  class Employee
+    {
+        public int Id{ get; set; }
+        public string Name{ get; set; }
+        public void ShowAllEmployee()
+        {
+         
+        }
+        public Employee SearchEmployeeByID(int id)
+        {
+            return new Employee();
+        }
+    }
+}
---
+++ WinReflectionDemo/WinReflectionDemo/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinReflectionDemo
+{
+    public partial class Form1 : Form
+    {
+        private object fileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string filePath = openFileDialog1.FileName;
+
+            Assembly assemblyObj = Assembly.LoadFrom(filePath);
+
+            Type[] myType = assemblyObj.GetTypes();
+            this.ReflectAll(myType[0]);
+            //Form1.ReflectAll(myType[0]);
+            //Type typeObj = typeof(Employee);
+            //ReflectAll(typeObj);
+
+
+
+        }
+        public void ReflectAll(Type typeObj)
+        {
+            // Getting all methods
+            MethodInfo[] methodList = typeObj.GetMethods();  
+
+            // Getting all properties
+            PropertyInfo[] propList = typeObj.GetProperties();
+
+            // Load all properties
+            foreach (var item in propList)
+            {
+                listBox1.Items.Add(item.Name);  
+            }
+
+            // Load all methods
+            foreach (var item in methodList)
+            {
+                listBox2.Items.Add(item.Name);  
+            }
+        }
+    }
+}
