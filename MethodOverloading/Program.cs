f8dd374300fb91e22d7430c8aba65bb6ae1baca2
Devika E S <devikaes17@gmail.com>
2026-01-15 15:51:25 +0530

    MethodOverloading

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MethodOverloading;
+class Program
+{
+    public static void Main()
+    {
+        Source sourceObj=new Source();
+        System.Console.WriteLine(sourceObj.Add(45,67,34));
+        System.Console.WriteLine(sourceObj.Add(9.76,67.98,23.49));
+    }
+}
---
+++ MethodOverloading/Source.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MethodOverloading;
+
+public class Source
+{
+    public int Add(int a,int b,int c)
+    {
+        return a+b+c;
+    }
+    public double Add(double a,double b,double c)
+    {
+        return a+b+c;
+    }
+}
