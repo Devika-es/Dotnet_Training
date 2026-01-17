8495faa1ba92ba2ee6ff61d696c53c766bbb0494
Devika E S <devikaes17@gmail.com>
2026-01-17 16:13:40 +0530

    17/1/26 Qn8 User Verification

---
+++ UserVerification/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+namespace UserVerification;
+class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            User userObj=new User();
+            System.Console.Write("Enter Name: ");
+            userObj.Name=Console.ReadLine();
+
+            System.Console.Write("Enter Phone Number: ");
+            userObj.PhoneNumber=Console.ReadLine();
+
+            Program programObj=new Program();
+            var q1=programObj.ValidatePhoneNumber(userObj.Name,userObj.PhoneNumber);
+            if(q1!=null)
+            {
+                System.Console.WriteLine("User data created successfully");
+            }
+
+        }
+        catch(InvalidPhoneNumberException ex)
+        {
+            System.Console.WriteLine(ex.Message);
+        }
+        catch(Exception ex)
+        {
+            System.Console.WriteLine(ex.Message);
+        }
+
+    }
+    public User ValidatePhoneNumber(string name,string phoneNumber)
+    {
+        User validateObj=new User();
+        
+        if(phoneNumber.Length==10)
+        {
+           bool isDigit=phoneNumber.All(char.IsDigit);
+           if(isDigit)
+            {
+                validateObj.Name=name;
+                validateObj.PhoneNumber=phoneNumber;
+            }
+            else
+            {
+                throw new InvalidPhoneNumberException();
+            }
+           
+        }
+        else
+        {
+            throw new InvalidPhoneNumberException();
+        }
+        return validateObj;
+    }
+}
+public class InvalidPhoneNumberException : Exception
+{
+    public InvalidPhoneNumberException():base()
+    {
+        System.Console.WriteLine("Invalid phone number");
+    }
+    public InvalidPhoneNumberException(string message):base(message)
+    {
+        System.Console.WriteLine(message);
+    }
+}
---
+++ UserVerification/User.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Web;
+
+namespace UserVerification;
+
+public class User
+{
+    public string Name { get; set; }
+    public string PhoneNumber{ get; set; }
+    
+}
