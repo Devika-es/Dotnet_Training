8e0b9cdad2af497afb82836a3a58f1f83c5fd155
Devika E S <devikaes17@gmail.com>
2026-01-17 15:28:25 +0530

    17/1/26 Qn5 EcommerceApplication

---
+++ EcommerceApplication/EcommerceShop.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace EcommerceApplication;
+
+public class EcommerceShop
+{
+    public string UserName{ get; set; }
+    public double WalletBalance { get; set; }
+    public double TotalPurchaseAmount { get; set; }
+}
---
+++ EcommerceApplication/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace EcommerceApplication;
+class Program
+{
+    public static void Main()
+    {
+        try{
+            EcommerceShop ecommerceObj=new EcommerceShop();
+            System.Console.Write("Enter user name: ");
+            ecommerceObj.UserName=Console.ReadLine();
+
+            System.Console.Write("Enter wallet balance: ");
+            ecommerceObj.WalletBalance=double.Parse(Console.ReadLine());
+
+            System.Console.Write("Enter total purchase amount: ");
+            ecommerceObj.TotalPurchaseAmount=double.Parse(Console.ReadLine());
+            
+            Program pObj=new Program();
+            var testObj=pObj.MakePayment(ecommerceObj.UserName, ecommerceObj.WalletBalance,ecommerceObj.TotalPurchaseAmount);
+            if(testObj!=null)
+            {
+                System.Console.WriteLine("Payment successful");
+            }
+        }
+        catch(Exception ex)
+        {
+            System.Console.WriteLine(ex.Message);
+        }
+      
+
+    }
+    public  EcommerceShop MakePayment(string name,double balance,double amount)
+    {
+        EcommerceShop ecommerceShop=new EcommerceShop();
+        ecommerceShop.UserName=name;
+        ecommerceShop.WalletBalance=balance;
+        ecommerceShop.TotalPurchaseAmount=amount;
+        if(balance<amount)
+        {
+            throw new InsufficientBalanceException();
+            
+        }
+        return ecommerceShop;
+        
+
+        
+    }
+}
+public class InsufficientBalanceException : Exception
+{
+    public InsufficientBalanceException():base()
+    {
+        System.Console.WriteLine("Insufficient balance in your digital wallet");
+    }
+}
