a486c62ee6934604f8e6f51e91b2fd29dc48c9d0
Devika E S <devikaes17@gmail.com>
2026-02-17 15:10:22 +0530

    Linq on Dictionary

---
+++ CityTemperatureBoard/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CityTemperatureBoard;
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> temperature = new Dictionary<string, int>
+        {
+            { "Chennai", 38 },
+            { "Delhi", 41 },
+            { "Bengaluru", 29 }
+        };
+
+        // TODO: Iterate and find max temperature city
+        var hottest=temperature.MaxBy(p=>p.Value);
+        Console.WriteLine($"{hottest.Key} - {hottest.Value}");
+    }
+}
