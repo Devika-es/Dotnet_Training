7cd1a074262ed00b47ef6e89221f18b5dc6ae279
Devika E S <devikaes17@gmail.com>
2026-02-17 14:53:43 +0530

    Linq on Dictionaries

---
+++ DepartmentBudget/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DepartmentBudget;
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, double> budget = new Dictionary<string, double>
+        {
+            { "IT", 12.5 },
+            { "HR", 6.2 },
+            { "Finance", 9.1 }
+        };
+
+        // TODO: Calculate and print total budget
+       Console.WriteLine($"Total Budget: {budget.Sum(p=>p.Value):F2}");
+    }
+}
